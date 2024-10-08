9e439c619e3c9e2cd1742fd8643687c9690685b5
e1b22069 <e1b22069@oit.ac.jp>
2024-10-08 14:18:57 +0900

    リポジトリの初期化

---
+++ src/main/java/team3/supercell/SupercellApplication.java
@@ -0,0 +1,13 @@
+package team3.supercell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team3/supercell/SupercellApplicationTests.java
@@ -0,0 +1,13 @@
+package team3.supercell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
